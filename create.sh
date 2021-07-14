7f72c8a679281b38bf41c31b3ae5e77e9b1c9fb7
Kevin de Ruijter <kevinderuijter@icloud.com>
2021-07-14 15:57:25 +0200

    Update create.sh
    Changed the image to a variable parameter.

--- create.sh
+++ create.sh
@@ -1,8 +1,9 @@
 # Creates a new container under specified port.
 # Usage:
-# ./create 8888
+# ./create 8888 7dfeaac661e4
 
 PORT=$1
+IMAGE=$2
 
 docker run \
 --env-file vpn_secrets.env \
@@ -11,4 +12,4 @@ docker run \
 -p $PORT:$PORT \
 --cap-add "NET_ADMIN" \
 --dns 1.1.1.1 \
-7dfeaac661e4
+$IMAGE
